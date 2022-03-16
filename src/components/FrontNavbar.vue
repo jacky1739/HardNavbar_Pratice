956e5e8ad6adfa57bf7bc404c383b66950db8029
Jacky <jacky123789456@gmail.com>
2022-03-16 11:44:14 +0800

    製作navbar

--- src/components/FrontNavbar.vue
+++ src/components/FrontNavbar.vue
@@ -1,3 +1,37 @@
 <template>
-  <h2>這是nav</h2>
+  <div class="navbar">
+    <div class="content">
+      <div class="logo">
+        <a href="#">CondingJacky</a>
+      </div>
+      <ul class="menu__list">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">Ffeature</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.navbar {
+  position: fixed;
+  z-index: 5;
+  width: 100%;
+  padding: .1rem 0;
+  background: #1b1b1b;
+  .logo a {
+    color: #fff;
+  }
+  .menu__list li a {
+    color: #fff;
+  }
+  .content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
+</style>
--- src/views/Front/IndexView.vue
+++ src/views/Front/IndexView.vue
@@ -1,3 +1,13 @@
 <template>
-  <h2>這是content</h2>
+  <div class="banner"></div>
+  <div class="about">
+    <div class="content">
+      <div class="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, earum.</div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab tenetur officiis assumenda? Ipsa possimus odit tempora error sunt inventore ipsam maxime facilis ad eum, amet eos temporibus incidunt modi eligendi ipsum reiciendis eveniet exercitationem cumque minus repudiandae illum alias quo esse. Autem possimus sequi laudantium aspernatur officiis maiores dicta molestiae voluptatibus eum doloribus quidem explicabo aliquid temporibus earum quia sapiente totam necessitatibus, quos facilis magnam inventore nisi blanditiis error. Quis repellat enim laudantium accusantium asperiores commodi id autem ut quo?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt dolores modi fugiat quos vitae adipisci recusandae. Culpa quas laudantium doloribus architecto? Officiis sequi minima similique soluta. Nisi delectus, corporis quaerat fugit voluptatem iure voluptatibus exercitationem tempore, voluptas, ut a hic architecto ipsum quam dicta? Quidem in maxime quisquam, excepturi accusamus fugiat voluptatum consequatur velit, at reprehenderit qui libero incidunt eius!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt dolores modi fugiat quos vitae adipisci recusandae. Culpa quas laudantium doloribus architecto? Officiis sequi minima similique soluta. Nisi delectus, corporis quaerat fugit voluptatem iure voluptatibus exercitationem tempore, voluptas, ut a hic architecto ipsum quam dicta? Quidem in maxime quisquam, excepturi accusamus fugiat voluptatum consequatur velit, at reprehenderit qui libero incidunt eius!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt dolores modi fugiat quos vitae adipisci recusandae. Culpa quas laudantium doloribus architecto? Officiis sequi minima similique soluta. Nisi delectus, corporis quaerat fugit voluptatem iure voluptatibus exercitationem tempore, voluptas, ut a hic architecto ipsum quam dicta? Quidem in maxime quisquam, excepturi accusamus fugiat voluptatum consequatur velit, at reprehenderit qui libero incidunt eius!</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  </div>
 </template>
