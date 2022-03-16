97124695c757ac11a3e37c6d613990e121aa0dd2
Jacky <jacky123789456@gmail.com>
2022-03-16 10:48:37 +0800

    project init

---
+++ src/views/Front/IndexView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>這是content</h2>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Navbar></Navbar>
+  <router-view></router-view>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/FrontNavbar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
